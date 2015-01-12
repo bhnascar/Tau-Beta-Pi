d7000c8c4e3c5024ba899790dff71d64b1aed22b
bhnascar <bhnascar@gmail.com>
2015-01-12 03:09:04 -0800

    Adding correct template name

--- page-templates/category.php
+++ page-templates/category.php
@@ -1,15 +1,11 @@
 <?php
 /**
- * The template for displaying all pages
- *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages and that other
- * 'pages' on your WordPress site will use a different template.
- *
- * @package WordPress
- * @subpackage Twenty_Thirteen
- * @since Twenty Thirteen 1.0
- */
+* Template Name:  Home
+*
+* @package WordPress
+* @subpackage Twenty_Thirteen
+* @since Twenty Thirteen 1.0
+*/
 
 get_header(); ?>
 
